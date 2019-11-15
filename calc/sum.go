9c8f8971007b4291c701d04cdbb74ff539b56de0
kaisa316 <490788241@qq.com>
2019-11-15 11:53:25 +0800

    go v3

--- calc/sum.go
+++ calc/sum.go
@@ -1,9 +1,5 @@
 package calc
 
-func Sum(a int, b int) int {
-	return a + b
-}
-
 //乘积计算
 func Mul(a int, b int) int {
 	return a * b
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kaisa316/nummanip/v2
+module github.com/kaisa316/nummanip/v3
 
 go 1.13
